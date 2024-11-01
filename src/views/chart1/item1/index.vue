059ec363a62eb9c2eecc406992764b34f11a7e77
FYM010101 <814116576@qq.com>
2024-11-01 17:14:28 +0800

    增加一个多图表组件

--- src/views/chart1/item1/index.vue
+++ src/views/chart1/item1/index.vue
@@ -1,104 +1,104 @@
 <template>
-    <div class="box5">
-        <div class="title">
-            <!-- <p>未来七天游客数量趋势图</p> -->
-            <!-- <img src="../../images/dataScreen-title.png" alt=""> -->
+    <!-- <el-scrollbar class="chart-list"> -->
+        <div class="row" v-for="(chartPair, index) in chartPairs" :key="index">
+            <div v-for="(chartData, idx) in chartPair" :key="idx" ref="chartRefs" class="chart"
+                :id="'chart' + (index * 2 + idx)"></div>
         </div>
-        <div class="charts" ref='line'></div>
-    </div>
+    <!-- </el-scrollbar> -->
 </template>
 
 <script setup lang="ts">
-import * as echarts from 'echarts';
-import { ref, onMounted } from 'vue';
-//获取图形图标的节点
-let line = ref();
-onMounted(() => {
-    let mycharts = echarts.init(line.value);
-    //设置配置项
-    mycharts.setOption({
-        //标题组件
-        title: {
-            text: '访问量'
-        },
-        //x|y轴
-        xAxis: {
-            type: 'category',
-            //两侧不留白
-            boundaryGap: false,
-            //分割线不要
-            splitLine: {
-                show: false
-            },
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-            //轴线的设置
-            axisLine: {
-                show: true
-            },
-            //刻度
-            axisTick: {
-                show: true
+import { ref, onMounted, onUnmounted, defineComponent } from 'vue'
+import * as echarts from 'echarts'
+
+interface ChartData {
+    name: string
+    data: number[]
+}
+const chartDataList = ref<ChartData[]>([
+    { name: '图表 1', data: [120, 200, 150, 80, 70, 110, 130] },
+    { name: '图表 2', data: [90, 160, 130, 170, 60, 180, 120] },
+    { name: '图表 3', data: [150, 230, 224, 218, 135, 147, 260] },
+    { name: '图表 4', data: [210, 290, 214, 218, 200, 240, 300] },
+    { name: '图表 5', data: [210, 290, 214, 218, 200, 240, 300] },
+    { name: '图表 6', data: [] },
+])
+
+const chartRefs = ref<(echarts.ECharts | null)[]>([])
+
+// 将数据分为每两个一组
+const chartPairs = ref<ChartData[][]>([])
+for (let i = 0; i < chartDataList.value.length; i += 2) {
+    chartPairs.value.push(chartDataList.value.slice(i, i + 2))
+}
+
+// 初始化图表
+const initCharts = () => {
+    chartDataList.value.forEach((chartData, index) => {
+        const chartDom = document.getElementById(`chart${index}`)
+        if(chartData.data.length == 0) return;
+        if (chartDom) {
+            const chartInstance = echarts.init(chartDom)
+            const option: echarts.EChartsOption = {
+                title: { text: chartData.name },
+                tooltip: { trigger: 'axis' },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+                },
+                yAxis: { type: 'value' },
+                series: [
+                    {
+                        name: chartData.name,
+                        type: 'line',
+                        data: chartData.data,
+                        smooth: true,
+                        areaStyle: {},
+                    },
+                ],
             }
-        },
-        yAxis: {
-            splitLine: {
-                show: false
-            },
-            //轴线的设置
-            axisLine: {
-                show: true
-            },
-            //刻度
-            axisTick: {
-                show: true
-            }
-        },
-        grid: {
-            left: 40,
-            top: 0,
-            right: 20,
-            bottom: 20
-        },
-        //系列
-        series: [
-            {
-                type: 'line',
-                data: [120, 1240, 66, 2299, 321, 890, 1200],
-                //平滑曲线的设置
-                smooth: true,
-                //区域填充样式
-                areaStyle: {
-                    color: {
-                        type: 'linear',
-                        x: 0,
-                        y: 0,
-                        x2: 0,
-                        y2: 1,
-                        colorStops: [{
-                            offset: 0, color: 'red' // 0% 处的颜色
-                        }, {
-                            offset: 1, color: 'blue' // 100% 处的颜色
-                        }],
-                        global: false // 缺省为 false
-                    }
-                }
-            }
-        ]
+            chartInstance.setOption(option)
+            chartRefs.value[index] = chartInstance
+        }
     })
+}
+
+// 监听窗口大小变化，调整图表尺寸
+const resizeCharts = () => {
+    chartRefs.value.forEach((chartInstance) => {
+        if (chartInstance) chartInstance.resize()
+    })
+}
+
+onMounted(() => {
+    initCharts()
+    window.addEventListener('resize', resizeCharts)
 })
+
+onUnmounted(() => {
+    window.removeEventListener('resize', resizeCharts)
+})
+
 </script>
 
-<style scoped lang="scss">
-.box5 {
-    width: 50%;
-    height: 50%;
-    // background: url(../../images/dataScreen-main-cb.png) no-repeat;
-    background-size: 100% 100%;
-    margin: 0px 20px;
+<style scoped>
+/* .chart-list {
+    height: 100vh; */
+    /* overflow-y: auto; */
+    /* padding: 10px; */
+    /* background-color: #f5f5f5;
+} */
 
-    .charts {
-        height: calc(100% - 40px);
-    }
+.row {
+    display: flex;
+    gap: 20px;
+    margin-bottom: 20px;
+}
 
+.chart {
+    flex: 1;
+    /* 每个图表平分父容器宽度 */
+    height: 400px;
 }
 </style>
